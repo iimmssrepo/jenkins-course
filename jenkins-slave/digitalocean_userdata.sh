4384f2659fb8acd1cec511116b8fa9ab03e11b10
User <email@example.com>
2020-03-20 08:54:11 +0100

    cambiado digitalocean_userdata.sh

--- jenkins-slave/digitalocean_userdata.sh
+++ jenkins-slave/digitalocean_userdata.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-apt-get update
-apt-get install -y docker-engine
+#apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+#apt-get update
+#apt-get install -y docker-engine
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 mkdir -p /var/jenkins_home/.ssh
-cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
+cp /administrador/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
 chmod 700 /var/jenkins_home/.ssh
 chmod 600 /var/jenkins_home/.ssh/authorized_keys
 chown -R 1000:1000 /var/jenkins_home
-docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d wardviaene/jenkins-slave
+docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d iimmssrepo/jenkins-slave
